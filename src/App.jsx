9e7e1474715f0780616c54b4509c5f0b171fa38f
Sivert Mathisen Hansen <sihaa049@osloskolen.no>
2026-03-14 13:11:16 +0100

    Project gallery sort working. Sort by newest or oldest.

--- src/App.jsx
+++ src/App.jsx
@@ -15,21 +15,9 @@ function Home() {
 function Projects() {
   const [projects, setProjects] = useState([
     {
-      id: 1,
-      title: "Stemmeteller for Erikstad",
-      img: "/assets/project_thumbnails/stemmeteller_erikstad_thumbnail.png",
-      github: "https://github.com/sivertmh/oppdrag-stemmeteller"
-    },
-    {
-      id: 2,
-      title: "Enkel Kalkulator",
-      img: "/assets/project_thumbnails/siverts_kalkulator.png",
-      github:
-        "https://github.com/sivertmh/hub-it1-prosjekter/tree/main/kalkulator",
-    },
-    {
       id: 3,
       title: "Karakterkalkulator",
+      date: new Date("2026-01-14"),
       img: "/assets/project_thumbnails/karakterkalk.png",
       github:
         "https://github.com/sivertmh/hub-it1-prosjekter/tree/main/karaktersnitt",
@@ -37,17 +25,65 @@ function Projects() {
     {
       id: 4,
       title: "The Ministry of Nothing",
+      date: new Date("2025-12-19"),
       img: "/assets/project_thumbnails/nothingministry_thumbnail.png",
       github: "https://github.com/sivertmh/flaskapp-nothingministry",
     },
+    {
+      id: 2,
+      title: "Enkel Kalkulator",
+      date: new Date("2025-12-3"),
+      img: "/assets/project_thumbnails/siverts_kalkulator.png",
+      github:
+        "https://github.com/sivertmh/hub-it1-prosjekter/tree/main/kalkulator",
+    },
+    {
+      id: 1,
+      title: "Stemmeteller for Erikstad",
+      date: new Date("2025-04-10"),
+      img: "/assets/project_thumbnails/stemmeteller_erikstad_thumbnail.png",
+      github: "https://github.com/sivertmh/oppdrag-stemmeteller",
+    },
   ]);
+
+  const reversedProjects = projects.reverse();
+
+  const sortByNewest = () => {
+    setProjects([...projects].sort((a, b) => b.date - a.date));
+  };
+
+  const sortByOldest = () => {
+    setProjects([...projects].sort((a, b) => a.date - b.date));
+  };
+
+  // Brukes i onChange hos #sort_proj.
+  // Sjekker verdi av valgt select og sorterer deretter.
+  const sortBy = (sortVal) => {
+    if (sortVal == "newest") {
+      sortByNewest();
+    } else {
+      sortByOldest();
+    }
+  };
 
   return (
     <main>
       <h1>Siste Prosjekter</h1>
-      <p>Her finner du mine siste prosjekter. Du kan lese mer om prosjektene ved å trykke på bildet.</p>
+      <p>
+        Her finner du mine siste prosjekter. Du kan lese mer om prosjektene ved
+        å trykke på bildet.
+      </p>
+      <select
+        onChange={() => sortBy(document.querySelector("#sort_proj").value)}
+        name="sort_proj"
+        id="sort_proj"
+      >
+        <option value="newest">Nyeste Først</option>
+        <option value="oldest">Eldste Først</option>
+      </select>
       <div className="project_wrapper">
-        {projects.map((proj) => {
+        {/* Lager et prosjektkort per objekt i projects-listen */}
+        {reversedProjects.map((proj) => {
           return (
             <a href={proj.github} target="blank_">
               <Projectcard key={proj.id} title={proj.title} img={proj.img} />
