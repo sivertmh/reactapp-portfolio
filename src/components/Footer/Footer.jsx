6756c06528eb668d80a36ef0010ea134e28dcde2
Sivert Mathisen Hansen <sihaa049@osloskolen.no>
2026-04-27 13:50:35 +0200

    Added to footer: logo and email.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,21 +1,35 @@
 import style from "./Footer.module.css";
+import logo from "../../logo_s.svg";
 
 const Footer = () => {
   return (
     <div className={style.footer}>
-      <div className={style["footer__github--wrapper"]}>
-        <a
-          className={style["footer__github"]}
-          href="https://github.com/sivertmh"
-          target="_blank"
-        >
+      <div>
+        <div className={style["footer__logo"]}>
           <img
-            className={style["footer__github--icon"]}
-            src={`${process.env.PUBLIC_URL}/assets/icons/GitHub_Invertocat_White.svg`}
-            alt="Logo for Github"
+            src={logo}
+            className={style.logo}
+            alt="Logo for Sivert M. Hansen"
           />
-          @sivertmh
-        </a>
+        </div>
+        <div className={style["footer__github--wrapper"]}>
+          <a
+            className={style["footer__github"]}
+            href="https://github.com/sivertmh"
+            target="_blank"
+          >
+            <img
+              className={style["footer__github--icon"]}
+              src={`${process.env.PUBLIC_URL}/assets/icons/GitHub_Invertocat_White.svg`}
+              alt="Logo for Github"
+            />
+            @sivertmh
+          </a>
+        </div>
+        <div className={style["footer__social"]}>
+          Epost:{" "}
+          <a href="mailto:sihaa049@osloskolen.no">sihaa049@osloskolen.no</a>
+        </div>
       </div>
     </div>
   );
